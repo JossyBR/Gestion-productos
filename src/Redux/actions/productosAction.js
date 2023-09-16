f4ba5193ea9f38ac26ef0f9168485346fd9e3b7e
Jossy Benitez <jossybr94@gmail.com>
2023-09-16 17:17:42 -0500

    Agregando producto con exito

--- src/Redux/actions/productosAction.js
+++ src/Redux/actions/productosAction.js
@@ -1,13 +1,32 @@
 import {
-    AGREGAR_PRODUCTO,
-    AGREGAR_PRODUCTO_EXITO,
-    AGREGAR_PRODUCTO_ERROR,
-  } from "../types";
+  AGREGAR_PRODUCTO,
+  AGREGAR_PRODUCTO_EXITO,
+  AGREGAR_PRODUCTO_ERROR,
+} from "../types";
 
+//Crear nuevos productos
+export function crearNuevoProducto(producto) {
+  return (dispatch) => {
+    dispatch(agregarProducto());
 
-  //Crear nuevos productos
-  export function crearNuevoProducto(producto) {
-    return () => {
-        console.log(producto)
+    try {
+      dispatch(agregarProductoExito(producto));
+    } catch (error) {
+      dispatch(agregarProductoError(true));
     }
-  }
+  };
+}
+
+const agregarProducto = () => ({
+  type: AGREGAR_PRODUCTO,
+  payload: true
+});
+
+//Si el producto se guarda en labase de datos
+const agregarProductoExito = (producto) => ({
+  type: AGREGAR_PRODUCTO_EXITO,
+  payload: producto,
+});
+
+//Si hubo un error
+const agregarProductoError = () => ({});
